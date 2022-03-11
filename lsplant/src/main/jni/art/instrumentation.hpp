cbad8eebf4b78eacdc8c7bae99c6cea95026d223
LoveSy <shana@zju.edu.cn>
2022-03-11 20:03:33 +0800

    Strip pending hook

--- lsplant/src/main/jni/art/instrumentation.hpp
+++ lsplant/src/main/jni/art/instrumentation.hpp
@@ -6,36 +6,46 @@
 namespace lsplant::art {
 
 class Instrumentation {
+    inline static ArtMethod *MaybeUseBackupMethod(ArtMethod *art_method, const void *quick_code) {
+        std::shared_lock lk(hooked_methods_lock_);
+        if (auto found = hooked_methods_.find(art_method);
+            found != hooked_methods_.end() && art_method->GetEntryPoint() != quick_code)
+            [[unlikely]] {
+            LOGD("Propagate update method code %p for hooked method %s to its backup", quick_code,
+                 art_method->PrettyMethod().c_str());
+            return found->second.second;
+        }
+        return art_method;
+    }
+
     CREATE_MEM_HOOK_STUB_ENTRY(
         "_ZN3art15instrumentation15Instrumentation21UpdateMethodsCodeImplEPNS_9ArtMethodEPKv", void,
         UpdateMethodsCodeImpl,
-        (Instrumentation * thiz, ArtMethod *art_method, const void *quick_code), {
-            if (IsHooked(art_method)) [[unlikely]] {
-                LOGD("Skip update method code for hooked method %s",
-                     art_method->PrettyMethod().c_str());
-                return;
-            } else {
-                backup(thiz, art_method, quick_code);
-            }
-        });
+        (Instrumentation * thiz, ArtMethod *art_method, const void *quick_code),
+        { backup(thiz, MaybeUseBackupMethod(art_method, quick_code), quick_code); });
 
     CREATE_MEM_HOOK_STUB_ENTRY(
         "_ZN3art15instrumentation15Instrumentation17UpdateMethodsCodeEPNS_9ArtMethodEPKv", void,
         UpdateMethodsCode, (Instrumentation * thiz, ArtMethod *art_method, const void *quick_code),
         {
-            if (IsHooked(art_method)) [[unlikely]] {
-                LOGD("Skip update method code for hooked method %s",
-                     art_method->PrettyMethod().c_str());
-                return;
+            if (UpdateMethodsCodeImpl.backup) {
+                UpdateMethodsCodeImpl.backup(thiz, MaybeUseBackupMethod(art_method, quick_code),
+                                             quick_code);
             } else {
-                backup(thiz, art_method, quick_code);
+                backup(thiz, MaybeUseBackupMethod(art_method, quick_code), quick_code);
             }
         });
 
 public:
     static bool Init(const HookHandler &handler) {
-        if (!HookSyms(handler, UpdateMethodsCodeImpl, UpdateMethodsCode)) {
+        int sdk_int = GetAndroidApiLevel();
+        if (!HookSyms(handler, UpdateMethodsCode)) {
             return false;
+        }
+        if (sdk_int >= __ANDROID_API_N__) [[likely]] {
+            if (!HookSyms(handler, UpdateMethodsCodeImpl)) {
+                return false;
+            }
         }
         return true;
     }
--- test/src/main/java/org/lsposed/lsplant/LSPTest.java
+++ test/src/main/java/org/lsposed/lsplant/LSPTest.java
@@ -25,4 +25,29 @@ public class LSPTest {
     String manyParametersMethod(String a, boolean b, byte c, short d, int e, long f, float g, double h, Integer i, Long j) {
         return a + b + c + d + e + f + g + h + i + j;
     }
+
+
+    public static class NeedInitialize {
+        static int x;
+
+        static {
+            x = 0;
+        }
+
+        static boolean staticMethod() {
+            try {
+                return x != 0;
+            } catch (Throwable e) {
+                return false;
+            }
+        }
+
+        static boolean callStaticMethod() {
+            try {
+                return staticMethod();
+            } catch (Throwable e) {
+                return false;
+            }
+        }
+    }
 }
