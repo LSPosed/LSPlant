85ab3c8c11e3c5b02b28756af40da52cbe2374ac
LoveSy <shana@zju.edu.cn>
2024-07-17 11:50:46 +0800

    Upgrade deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.lsplugin.publish)
 }
 
-val androidTargetSdkVersion by extra(34)
+val androidTargetSdkVersion by extra(35)
 val androidMinSdkVersion by extra(21)
-val androidBuildToolsVersion by extra("34.0.0")
-val androidCompileSdkVersion by extra(34)
-val androidNdkVersion by extra("27.0.11902837-rc1")
+val androidBuildToolsVersion by extra("35.0.0")
+val androidCompileSdkVersion by extra(35)
+val androidNdkVersion by extra("27.0.12077973")
 val androidCmakeVersion by extra("3.28.0+")
