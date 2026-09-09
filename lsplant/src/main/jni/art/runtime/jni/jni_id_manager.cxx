6cd0134e68519735c21f32410357f301bd55faff
yucong.park <yucong.park@bytedance.com>
2026-09-09 22:22:03 +0800

    bugfix: fix crash on android 17 gsi (CP41.260701.005) with new art EncodeGenericId

--- lsplant/src/main/jni/art/runtime/jni/jni_id_manager.cxx
+++ lsplant/src/main/jni/art/runtime/jni/jni_id_manager.cxx
@@ -26,11 +26,24 @@ private:
         return backup(thiz, method);
     };
 
+    inline static auto EncodeGenericIdWithClass_ =
+        "_ZN3art3jni12JniIdManager15EncodeGenericIdINS_9ArtMethodEEEmNS_6HandleINS_6mirror5ClassEEENS_16ReflectiveHandleIT_EE"_sym
+            .hook
+            ->*[]<MemBackup auto backup>(JniIdManager *thiz,
+                                         Handle<mirror::Class> klass,
+                                         ReflectiveHandle<ArtMethod> method) static -> uintptr_t {
+        if (auto *target = IsBackup(method.Get()); target) {
+            LOGD("get generic id for %s", method.Get()->PrettyMethod().c_str());
+            method.Set(target);
+        }
+        return backup(thiz, klass, method);
+    };
+
 public:
     static bool Init(JNIEnv *env, const HookHandler &handler) {
         int sdk_int = GetAndroidApiLevel();
         if (sdk_int >= kSdkR) {
-            if (IsJavaDebuggable(env) && !handler(EncodeGenericId_)) {
+            if (IsJavaDebuggable(env) && !handler(EncodeGenericId_, EncodeGenericIdWithClass_)) {
                 LOGW("Failed to hook EncodeGenericId, attaching debugger may crash the process");
             }
         }
